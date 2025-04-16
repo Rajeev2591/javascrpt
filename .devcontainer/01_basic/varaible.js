f786f00d7e2792658240cbcf89560c0a4e3395bc
Rajeev2591 <31560420+Rajeev2591@users.noreply.github.com>
2025-04-16 17:48:23 +0000

    dataType

---
+++ .devcontainer/01_basic/varaible.js
@@ -0,0 +1,11 @@
+const accountId=12345;
+let accountEmail= "rhc.com";
+var accountPassowrd= "234567";
+accountCity= "Banglore";
+let accountState;
+console.log(accountId)
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail,accountPassowrd,accountCity,accountState])
